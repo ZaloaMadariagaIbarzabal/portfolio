b0b5254e33cb77b570fbe7a22819ba30500b2add
zmadariaga <zmadariaga@gmail.com>
2026-09-02 00:15:13 +0200

    Delete Eskuetan2023 directory

--- Eskuetan2023/src/components/Toast.jsx
+++
@@ -1,6 +0,0 @@
-import { useShop } from '../context/ShopContext';
-
-export default function Toast() {
-  const { toast } = useShop();
-  return <div className={`toast ${toast ? 'show' : ''}`}>{toast}</div>;
-}
--- Eskuetan2023/src/data/pImg.js
+++
@@ -1,7 +0,0 @@
-import { IMG_MAP } from './imageMap';
-
-const FALLBACK = 'https://images.pexels.com/photos/16891088/pexels-photo-16891088.jpeg?auto=compress&cs=tinysrgb&w=1600';
-
-export function pImg(product) {
-  return IMG_MAP[product.img] || FALLBACK;
-}
